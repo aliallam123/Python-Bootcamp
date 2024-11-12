b92a8f6340eb80f5f8f28e9931cda70eac8f692a
Ali Allam <146081536+aliallam123@users.noreply.github.com>
2024-11-12 21:46:56 +0000

    Update empty_file.py

--- Section 14 - Coming Soon/empty_file.py
+++ Section 14 - Coming Soon/empty_file.py
@@ -1,5 +1,26 @@
 To-do:
 
+7:00: Fajr
+
+8:00-9:00: Breakfast
+
+9:00-10:00: Hifz Makharij
+
+10:00-12:00: Advanced Python Modules (as much as you can do)
+
+12:00-1:00: Gym
+
+1:00-2:00: Lunch
+
+2:00-5:00: Solutions and Development Quality Report
+
+6:00-7:00: Dinner
+
+8:00-9:00 Make Recitersoftheharam post
+
+9:00: Chill
+To-do:
+
 12:00-12:30: Plan + Sali
 
 12:30-1:30: Python Bootcamp
